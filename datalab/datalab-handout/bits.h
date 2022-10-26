b5cb30368fe09b39b39ceefeda7c42a318cf90c4
faii <1320407956@qq.com>
2022-10-26 21:50:18 +0800

    maybe?

--- datalab/datalab-handout/bits.h
+++ datalab/datalab-handout/bits.h
@@ -1,31 +1,31 @@
-
-int bitAnd(int, int);
-int test_bitAnd(int, int);
-int getByte(int, int);
-int test_getByte(int, int);
-int logicalShift(int, int);
-int test_logicalShift(int, int);
-int bitCount(int);
-int test_bitCount(int);
-int bang(int);
-int test_bang(int);
+//1
+int bitXor(int, int);
+int test_bitXor(int, int);
 int tmin();
 int test_tmin();
-int fitsBits(int, int);
-int test_fitsBits(int, int);
-int divpwr2(int, int);
-int test_divpwr2(int, int);
+//2
+int isTmax(int);
+int test_isTmax(int);
+int allOddBits();
+int test_allOddBits();
 int negate(int);
 int test_negate(int);
-int isPositive(int);
-int test_isPositive(int);
+//3
+int isAsciiDigit(int);
+int test_isAsciiDigit(int);
+int conditional(int, int, int);
+int test_conditional(int, int, int);
 int isLessOrEqual(int, int);
 int test_isLessOrEqual(int, int);
-int ilog2(int);
-int test_ilog2(int);
-unsigned float_neg(unsigned);
-unsigned test_float_neg(unsigned);
-unsigned float_i2f(int);
-unsigned test_float_i2f(int);
-unsigned float_twice(unsigned);
-unsigned test_float_twice(unsigned);
+//4
+int logicalNeg(int);
+int test_logicalNeg(int);
+int howManyBits(int);
+int test_howManyBits(int);
+//float
+unsigned floatScale2(unsigned);
+unsigned test_floatScale2(unsigned);
+int floatFloat2Int(unsigned);
+int test_floatFloat2Int(unsigned);
+unsigned floatPower2(int);
+unsigned test_floatPower2(int);
--- datalab/datalab-handout/tests.c
+++ datalab/datalab-handout/tests.c
@@ -25,97 +25,94 @@ unsigned f2u(float f) {
   return a.u;
 }
 
-int test_bitAnd(int x, int y)
+//1
+int test_bitXor(int x, int y)
 {
-  return x&y;
-}
-int test_getByte(int x, int n)
-{
-    unsigned char byte;
-    switch(n) {
-    case 0:
-      byte = x;
-      break;
-    case 1:
-      byte = x >> 8;
-      break;
-    case 2:
-      byte = x >> 16;
-      break;
-    default:
-      byte = x >> 24;
-      break;
-    }
-    return (int) (unsigned) byte;
-}
-int test_logicalShift(int x, int n) {
-  unsigned u = (unsigned) x;
-  unsigned shifted = u >> n;
-  return (int) shifted;
-}
-int test_bitCount(int x) {
-  int result = 0;
-  int i;
-  for (i = 0; i < 32; i++)
-    result += (x >> i) & 0x1;
-  return result;
-}
-int test_bang(int x)
-{
-  return !x;
+  return x^y;
 }
 int test_tmin(void) {
   return 0x80000000;
 }
-int test_fitsBits(int x, int n)
-{
-  int TMin_n = -(1 << (n-1));
-  int TMax_n = (1 << (n-1)) - 1;
-  return x >= TMin_n && x <= TMax_n;
+//2
+int test_isTmax(int x) {
+    return x == 0x7FFFFFFF;
 }
-int test_divpwr2(int x, int n)
-{
-    int p2n = 1<<n;
-    return x/p2n;
+int test_allOddBits(int x) {
+  int i;
+  for (i = 1; i < 32; i+=2)
+      if ((x & (1<<i)) == 0)
+   return 0;
+  return 1;
 }
+
+
+
+
+
+
+
 int test_negate(int x) {
   return -x;
 }
-int test_isPositive(int x) {
-  return x > 0;
+//3
+
+
+int test_isAsciiDigit(int x) {
+  return (0x30 <= x) && (x <= 0x39);
+}
+int test_conditional(int x, int y, int z)
+{
+  return x?y:z;
 }
 int test_isLessOrEqual(int x, int y)
 {
   return x <= y;
 }
-int test_ilog2(int x) {
-  int mask, result;
-  /* find the leftmost bit */
-  result = 31;
-  mask = 1 << result;
-  while (!(x & mask)) {
-    result--;
-    mask = 1 << result;
-  }
-  return result;
+//4
+int test_logicalNeg(int x)
+{
+  return !x;
 }
-unsigned test_float_neg(unsigned uf) {
-    float f = u2f(uf);
-    float nf = -f;
-    if (isnan(f))
- return uf;
-    else
- return f2u(nf);
+int test_howManyBits(int x) {
+    unsigned int a, cnt;
+    x = x<0 ? -x-1 : x;
+    a = (unsigned int)x;
+    for (cnt=0; a; a>>=1, cnt++)
+        ;
+
+    return (int)(cnt + 1);
 }
-unsigned test_float_i2f(int x) {
-  float f = (float) x;
-  return f2u(f);
-}
-unsigned test_float_twice(unsigned uf) {
+//float
+unsigned test_floatScale2(unsigned uf) {
   float f = u2f(uf);
   float tf = 2*f;
   if (isnan(f))
     return uf;
   else
     return f2u(tf);
+}
+int test_floatFloat2Int(unsigned uf) {
+  float f = u2f(uf);
+  int x = (int) f;
+  return x;
+}
+unsigned test_floatPower2(int x) {
+  float result = 1.0;
+  float p2 = 2.0;
+  int recip = (x < 0);
+  /* treat tmin specially */
+  if ((unsigned)x == 0x80000000) {
+      return 0;
+  }
+  if (recip) {
+    x = -x;
+    p2 = 0.5;
+  }
+  while (x > 0) {
+    if (x & 0x1)
+      result = result * p2;
+    p2 = p2 * p2;
+    x >>= 1;
+  }
+  return f2u(result);
 }
